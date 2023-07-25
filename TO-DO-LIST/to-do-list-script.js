b645a60ee6ca8c002a0138383f2871a3f948a64b
Wai Yan <waiyantun2k22@gmail.com>
2023-07-25 16:48:50 +0800

    adding to-do-list project

---
+++ TO-DO-LIST/to-do-list-script.js
@@ -0,0 +1,37 @@
+const todoList = [];
+renderToDoList();
+
+function renderToDoList(){
+    let toDoListHTML = [];
+
+    for (let i=0; i< todoList.length; i++){
+        const toDoObject = todoList[i];
+        const {name, dueDate} = toDoObject;
+
+        const html = `
+        <div>${name}</div> <div> ${dueDate} </div> 
+        <button onclick="
+            todoList.splice(${i}, 1);
+            renderToDoList();">Delete</button> 
+
+        `;
+
+        toDoListHTML += html;
+    }
+    document.querySelector('.todo-list').innerHTML = toDoListHTML;
+}
+
+function addToDo(){
+    const inputElement = document.querySelector('.name-input');
+    const name  = inputElement.value;
+
+    const dateElement = document.querySelector('.dueDate-input');
+    const dueDate = dateElement.value;
+    todoList.push({name, dueDate})
+    
+    inputElement.value = '';
+    dateElement.value='';
+    renderToDoList();
+    
+    
+}
