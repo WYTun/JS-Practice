f6c0e1c3aaa439669ffd244ec1013c47aea48500
Wai Yan <waiyantun2k22@gmail.com>
2023-07-27 13:41:11 +0800

    adding done button

--- TO-DO-LIST/to-do-list-script.js
+++ TO-DO-LIST/to-do-list-script.js
@@ -13,6 +13,9 @@ function renderToDoList(){
         <button class="delete" onclick="
             todoList.splice(${index}, 1);
             renderToDoList();">Delete</button> 
+        <button class="done" onclick="
+            done();
+            renderToDoList();">DONE</button> 
         `;
 
         toDoListHTML += html;  
@@ -27,12 +30,26 @@ function addToDo(){
     const name  = inputElement.value;
 
     const dateElement = document.querySelector('.dueDate-input');
-    const dueDate = dateElement.value;
+    const today = new Date().toISOString().split('T')[0];
+    let dueDate = dateElement.value;
+    if (dueDate === ''){
+        dueDate = today;
+    }
+
     todoList.push({name, dueDate})
-    
     inputElement.value = '';
     dateElement.value='';
-    renderToDoList();
+    renderToDoList(); 
+}
+
+
+function done(){
+    console.log('done');
+    const toDoName = document.querySelector('.name');
+    const toDoDate = document.querySelector('.date');
+    const deleteButton = document.querySelector('delete');
+    const doneButton = document.querySelector('done');
+
     
-    
+
 }
